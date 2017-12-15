fe64b08800e23121f4412a13c393981b59debc4a
Unknown <eiwicki@gwo.pl>
2017-12-15 13:52:27 +0100

    redux

---
+++ expensify-app/resources/src/js/playground/redux_101.js
@@ -0,0 +1,64 @@
+import { createStore } from 'redux';
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy 
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy 
+});
+
+const reset = ({ reset = 0 } = {}) => ({
+  type: 'RESET',
+  reset 
+});
+
+const set = ({ set = 101 } = {}) => ({
+  type: 'SET',
+  set 
+});
+
+const countReducer = (state = { count: 0 }, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.incrementBy
+      }; 
+      break;
+    case 'DECREMENT':
+      return {
+        count: state.count - action.decrementBy
+      }; 
+      break;
+    case 'SET':
+      return {
+        count: action.set
+      };
+    case 'RESET':
+      return {
+        count: action.reset
+      }; 
+    default:
+      return state;
+  }
+}
+
+const store = createStore(countReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(incrementCount());
+
+store.dispatch(incrementCount({ incrementBy: 5 }));
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 10 }));
+
+store.dispatch(reset({ count: 0 }));
+
+store.dispatch(set({ count: 101 }));
---
+++ expensify-app/resources/src/js/playground/redux_expensify.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from 'redux';
+
+const demoState = {
+  expenses: [{
+    id: 'test',
+    description: 'test2',
+    note: 'test3',
+    amount: 54500,
+    createdAt: 0 
+  }],
+  
+};
